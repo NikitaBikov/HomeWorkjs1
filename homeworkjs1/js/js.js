70d8d379a04c0cc451955d81600ff4af109d8b6c
NikitaBikov <51048723+NikitaBikov@users.noreply.github.com>
2019-06-16 22:53:17 +0300

    Add files via upload

---
+++ homeworkjs1/js/js.js
@@ -0,0 +1,34 @@
+"use strict";
+
+function validateNumber(userAge) {
+    return (userAge < 1 || userAge > 110 ||(isNaN(userAge) ));
+}
+
+function validateName(userName) {
+    return (userName === null  || userName === "");
+}
+
+let userName = prompt('Enter your name. ');
+while (validateName(userName)) {
+    userName = prompt("Please, entered your name if you forgot or check the correct entered your name", userName);
+}
+let userAge = prompt('Enter your age. ');
+while (validateNumber(userAge)) {
+    userAge = prompt("Please, check the correct entered information", userAge);
+}
+
+let urAllowed = 'Welcome, ';
+let urNotAllowed = 'You are not allowed to visit this website.';
+let areUsure = 'Are you sure you want to continue?';
+if (userAge > 22) {
+    alert(urAllowed + userName)
+} else if (userAge >= 18 && userAge <= 22) {
+    let result = confirm(areUsure);
+    if (result) {
+        alert(urAllowed + userName)
+    } else {
+        alert(urNoTAllowed)
+    }
+} else {
+    alert(urNotAllowed)
+}
---
+++ homeworkjs5/js/js.js
@@ -0,0 +1,26 @@
+"use strict";
+//считаем число Фиббоначи
+let firstNumber = +(parseInt(prompt('Enter your first number : ', '')));
+let secondNumber = +(parseInt(prompt('Enter your second number : ', '')));
+let n = +(parseInt(prompt('Enter sequence number: ', '')));
+
+while (isNaN(firstNumber) || isNaN(secondNumber)) {
+    firstNumber = +(parseInt(prompt('Please,enter your first number: ', `${firstNumber}`)));
+    secondNumber = +(parseInt(prompt('Please,enter your second number: ', `${secondNumber}`)));
+    n = +(parseInt(prompt('Please,enter sequence number: ', `${n}`)));
+}
+function fib(firstNumber, secondNumber, n){
+    if(n>=0) {
+        if (n==1) return firstNumber;
+        else if (n==2) return secondNumber;
+        else return fib(firstNumber, secondNumber, n - 1) + fib(firstNumber, secondNumber, n - 2);
+    }
+    else {
+        if (n==-1) return firstNumber;
+        else if (n==-2) return firstNumber;
+        else return fib(firstNumber, secondNumber, n+ 1) + fib(firstNumber, secondNumber, n + 2);
+    }
+}
+let resultFib=fib(firstNumber,secondNumber,n);
+alert(resultFib);
+``
